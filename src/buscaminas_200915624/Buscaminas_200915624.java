78b0f68a70fd4f3859cba5cde56cea65f7029eac
Estuardo Ardón <Estuardo Ardn@HP-ARDON>
2017-08-23 17:18:32 -0600

    Práctica 1 - Inicio

---
+++ src/buscaminas_200915624/Buscaminas_200915624.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas_200915624;
+
+/**
+ *
+ * @author Estuardo Ardón
+ */
+public class Buscaminas_200915624 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
