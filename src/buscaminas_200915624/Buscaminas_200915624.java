b7da5243ec1a14c7f398b105ba0ad36a45e30ab2
Estuardo Ardón <Estuardo Ardn@HP-ARDON>
2017-08-23 17:52:10 -0600

    Realización de menús en consola

--- src/buscaminas_200915624/Buscaminas_200915624.java
+++ src/buscaminas_200915624/Buscaminas_200915624.java
@@ -5,9 +5,11 @@
  */
 package buscaminas_200915624;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Estuardo Ardón
+ * @author Oscar Estuardo Ardón Castillo
  */
 public class Buscaminas_200915624 {
 
@@ -16,6 +18,46 @@ public class Buscaminas_200915624 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner inic = new Scanner(System.in);
+        boolean salir = false;
+        String opini;
+        
+        System.out.println("\nOscar Estuardo Ardón Castillo");
+        System.out.println("Carnet: 200915624");
+        System.out.println("Práctica 1 - IPC1 ''A'' ");
+        System.out.println("\n*** BUSCAMINAS ***\n");
+        System.out.println("-- Presione enter para iniciar --");
+        opini = inic.nextLine();
+        
+        while(!salir) { 
+            int opcion;
+            Scanner opc = new Scanner(System.in);
+                       
+            System.out.println("*** BUSCAMINAS ***\n");
+            System.out.println("1. Principiante");
+            System.out.println("2. Intermedio");
+            System.out.println("3. Experto");
+            System.out.println("4. Salir\n");
+            System.out.println("Elija una opción");
+            opcion = opc.nextInt();
+            
+            switch(opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Ingrese un número entre 1 y 4.");
+            }
+        }
     }
     
 }
